c088fca7606f92e291b5006c5d486a014b81cf2d
Megan Ellman <meganlellman@gmail.com>
2023-01-29 14:01:15 -0800

    fixed post, added a / in the file path

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,34 +1,24 @@
 const api = require('express').Router();
 const fs = require('fs');
-const uuid = require('../helpers/uuid');
 
 api.get('/', (req, res) => {
     console.info(`${req.method} request received to add a note`);
-    const file = fs.readFileSync('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const note = JSON.parse(data);
-            writeToFile('./db/db.json', note);
-        }
-    })
+    const file = fs.readFileSync('./db/db.json', 'utf8')
     res.send(file)
 });
 
 api.post('/', (req, res) => {
     console.info(`${req.method} request received to add a note`);
 
-    console.log(`req is ${req}`);
     const { title, text } = req.body;
 
     if (title && text) {
         const newNote = {
             title,
             text,
-            review_id: uuid()
         };
 
-        let notes = fs.readFileSync('../../db/db.json');
+        let notes = fs.readFileSync('./db/db.json', 'utf8');
 
         notes = JSON.parse(notes);
 
@@ -36,8 +26,8 @@ api.post('/', (req, res) => {
 
         const noteString = JSON.stringify(notes);
 
-        fs.writeFile('../../db/db.json', noteString, (err) =>
-            err ? console.error(err) : console.log('New note has been added.'));
+        fs.writeFile('./db/db.json', noteString, (err) => 
+        err ? console.error(err) : console.log('New note has been added.'));
 
         const response = {
             status: 'success',
@@ -45,11 +35,10 @@ api.post('/', (req, res) => {
         };
 
         console.log(response);
-        res.send(response);
         res.status(201).json(response);
     } else {
-        res.status(500).json('Error in posting note');
-    }
+    res.status(500).json('Error in posting note');
+}
 });
 
 module.exports = api;
