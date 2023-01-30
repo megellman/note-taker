0277f597c3ff072b18094333356905ba300a34bf
Megan Ellman <meganlellman@gmail.com>
2023-01-29 16:09:40 -0800

    changed local host

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const api = require('express').Router();
 const fs = require('fs');
+const uuid = require('../helpers/uuid')
 
 api.get('/', (req, res) => {
     console.info(`${req.method} request received to add a note`);
@@ -16,6 +17,7 @@ api.post('/', (req, res) => {
         const newNote = {
             title,
             text,
+            id: uuid()
         };
 
         let notes = fs.readFileSync('./db/db.json', 'utf8');
@@ -26,8 +28,8 @@ api.post('/', (req, res) => {
 
         const noteString = JSON.stringify(notes);
 
-        fs.writeFile('./db/db.json', noteString, (err) => 
-        err ? console.error(err) : console.log('New note has been added.'));
+        fs.writeFile('./db/db.json', noteString, (err) =>
+            err ? console.error(err) : console.log('New note has been added.'));
 
         const response = {
             status: 'success',
@@ -37,8 +39,24 @@ api.post('/', (req, res) => {
         console.log(response);
         res.status(201).json(response);
     } else {
-    res.status(500).json('Error in posting note');
-}
+        res.status(500).json('Error in posting note');
+    }
 });
+
+api.delete('/:id', (req, res) => {
+    if (req.params.id) {
+        console.info(`${req.method} request received to remove a note`);
+        let notes = fs.readFileSync('./db/db.json', 'utf8')
+        notes = JSON.parse(notes);
+        const id = req.params.id;
+        let noteString = notes.filter(note => note.id !== id)
+        let newNoteString = JSON.stringify(noteString);
+        console.log(noteString);
+        fs.writeFile('./db/db.json', newNoteString, (err) =>
+            err ? console.error(err) : console.log('Note has been removed.'));
+    } else {
+        res.status(400).send('Note ID not provided');
+    }
+})
 
 module.exports = api;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -23,5 +23,5 @@ app.get('*', (req, res) =>
 );
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at http://0.0.0.0:${PORT} 🚀`)
 );
